9256e5108332abdb2ad64668b8ffc5654fce0370
Jason Bongiovanni <jpbon@protonmail.com>
2022-08-17 10:10:35 -0400

    updates

--- scraper/views.py
+++ scraper/views.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") or config("GOOGLE_CHROME_BIN")
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(executable_path=r'C:/selenium_drivers/chromedriver.exe' or os.environ.get("CHROMEDRIVER_PATH") or config('CHROMEDRIVER_PATH'), chrome_options=options)
